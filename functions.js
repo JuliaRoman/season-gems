17f4eed7bca2d90b97f5121005cf356ca34a82c1
RomaniJulia <73263268+JuliaRoman@users.noreply.github.com>
2021-10-23 00:59:28 -0300

    ganhando_forma
    Versão contém:
    Menu atualizado
    Página da equipe (quase finalizada)
    Contato (quase pronto)
    FAQ (Faltando apenas conteúdo)

---
+++ functions.js
@@ -0,0 +1,19 @@
+//by Julia Romani Barreta
+
+// Accordion - FAQ 
+function accordion(){ /* Função para ativar e desativar accordion */
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
+}
