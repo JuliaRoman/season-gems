665ec48d488c610d4bf71bb48ea535fc7e74919d
RomaniJulia <73263268+JuliaRoman@users.noreply.github.com>
2021-10-27 01:55:00 -0300

    final-personagem
    Finalização de página de personagem.

--- functions.js
+++ functions.js
@@ -17,3 +17,19 @@ function accordion(){ /* Função para ativar e desativar accordion */
 		});
 	}
 }
+
+/*** Mudança de classe - menu ***/
+function trocaMenu(){
+			
+		window.addEventListener('scroll', function (e) {
+		var nav = document.querySelector('.barra-nav');
+        
+		if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+            nav.classList.add('nav-colored');
+            nav.classList.remove('nav-transparent');
+        } else {
+            nav.classList.add('nav-transparent');
+            nav.classList.remove('nav-colored');
+        }
+    });
+}
